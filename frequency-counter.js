7262b981074de62e31bd8e1adca7e5dcf1fbfa23
Abhishek Kumar <abhishekkumar@MacBook-Pro.local>
2021-12-08 20:17:12 +0900

    Removing console and adding problem statement

--- frequency-counter.js
+++ frequency-counter.js
@@ -1,10 +1,13 @@
+// Problem - Create a function same, which accepts two arrays.
+// The function should return true if every value in the array has
+// it's corresponding value squared in the second array.
+// The frequency of values must be same
 function same(arr1, arr2) {
     if (arr1.length !== arr2.length) {
         return false;
     }
     // arr1.forEach((element) => {
     //     correctIndex = arr2.indexOf(element ** 2);
-    //     console.log(correctIndex);
     //     if (correctIndex === -1) {
     //         return false;
     //     }
